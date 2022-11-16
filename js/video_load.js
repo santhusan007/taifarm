1d49d9e5644c0820a094189886fb338e7ec25cd1
santhosh <511santhosh@gmail.com>
2022-11-16 11:15:11 +0530

    gif

---
+++ js/video_load.js
@@ -0,0 +1,9 @@
+(function ($) {
+  
+    "use strict";
+  
+      // PRE LOADER
+      $(window).load(function(){
+        $('.preloader').delay(3000).slideUp('slow'); // set duration in brackets    
+      });
+    })(window.jQuery);
